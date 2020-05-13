859e00c5338cadf963023e01db5fd4964cfc6cb5
Max Tang <tw7613781@gmail.com>
2020-05-13 11:46:05 +0900

    feat: forget to commit the code

--- trace/tracer.go
+++ trace/tracer.go
@@ -1,13 +1,34 @@
 package trace
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 // Tracer is the interface that describes an object capable of tracing events throughout code.
 type Tracer interface {
 	Trace(...interface{})
 }
 
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	fmt.Fprint(t.out, a...)
+	fmt.Fprintln(t.out)
+}
+
+type nilTracer struct{}
+
+func (t *nilTracer) Trace(a ...interface{}) {}
+
+// Off creates a Tracer that will ignore calls to Trace
+func Off() Tracer {
+	return &nilTracer{}
+}
+
 // New is unknown
 func New(w io.Writer) Tracer {
-	return nil
+	return &tracer{out: w}
 }
